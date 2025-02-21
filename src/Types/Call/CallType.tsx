d58c94494a4156f0d4afc41722183076aab27c20
MosesKal <mosesziongo@gmail.com>
2025-02-21 16:19:46 +0200

    feat: implement modal create role

---
+++ src/Types/Call/CallType.tsx
@@ -0,0 +1,36 @@
+export interface CallInstance {
+    id: string;
+    name: string;
+    created_at: string;
+    updated_at: string;
+    deleted_at: string;
+    description: string;
+    ended_at: string;
+    started_at: string;
+    published_at: string;
+    cover: string | null;
+    document: string | null;
+    form: Object | null;
+    reviewers: string | null;
+    requirements: Object | null;
+}
+
+export interface Author {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    deleted_at: string;
+    email: string;
+    name: string;
+    password: string;
+    phone_number: string;
+    address: string;
+    google_image: string | null;
+    profile: string | null;
+    verified_at: string;
+}
+
+export interface CallType extends  CallInstance {
+    authors: Author;
+}
+
