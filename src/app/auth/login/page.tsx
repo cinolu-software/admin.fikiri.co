4000cb598507ef09117cc8b110c51c3fbb717c91
MosesKal <mosesziongo@gmail.com>
2025-03-31 11:29:58 +0200

    feat: rediction app

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -17,51 +17,46 @@ const UserLogin = () => {
         dispatch(getProfile())
     }, [dispatch]);
 
-    // const handleRoleBasedRedirection = (roles: string[]) => {
+    const handleRoleBasedRedirection = (roles: string[]) => {
 
-    //     if (roles.includes('admin')) {
-    //         return '/admin/homeAdmin';
-    //     } else if (roles.includes('cartograph')) {
-    //         return '/cartograph/homeCartograph';
-    //     } else if (roles.includes('experimentor')) {
-    //         return '/experimentor/homeExperimentor';
-    //     } else if (roles.includes('explorator')) {
-    //         return '/explorator/homeExplorator';
-    //     } else if (roles.includes('user')) {
-    //         return '/user/homeUser';
-    //     }
+        if (roles.includes('admin')) {
+            return '/admin/homeAdmin';
+        } else if (roles.includes('cartograph')) {
+            return '/cartograph/homeCartograph';
+        } else if (roles.includes('experimentor')) {
+            return '/experimentor/homeExperimentor';
+        } else if (roles.includes('explorator')) {
+            return '/explorator/homeExplorator';
+        } else if (roles.includes('user')) {
+            return '/user/homeUser';
+        }
 
-    //     return process.env.NEXT_PUBLIC_HOST_CLIENT as string;
-    // };
+        return process.env.NEXT_PUBLIC_HOST_CLIENT as string;
+    };
 
-    // useEffect(
-    //     () => {
-    //         if(statusAuthentication === "succeeded" && isAuthenticated && userData){
-    //             const redirectPath = handleRoleBasedRedirection(userData.roles);
-    //             // router.push(redirectPath);
-    //             router.push('/admin/homeAdmin');
-    //         }else if(statusAuthentication === 'failed'){
-    //             router.push(process.env.NEXT_PUBLIC_HOST_CLIENT as string);
-    //         }
-    //     }, 
-    // [statusAuthentication, isAuthenticated, userData, router]
-    // );
-
-    useEffect(() => {
-        console.log("statusAuthentication", statusAuthentication, "isAuthenticated", isAuthenticated, "userData", userData);
-    }, [statusAuthentication, isAuthenticated, userData]);
+    useEffect(
+        () => {
+            if(statusAuthentication === "succeeded" && isAuthenticated && userData){
+                const redirectPath = handleRoleBasedRedirection(userData.roles);
+                router.push(redirectPath);
+            }else if(statusAuthentication === 'failed'){
+                router.push(process.env.NEXT_PUBLIC_HOST_CLIENT as string);
+            }
+        }, 
+    [statusAuthentication, isAuthenticated, userData, router]
+    );
 
   return (
     <Container fluid className="p-0">
       <Row className="m-0">
         <Col xs="12" className="p-0">
           <div className="login-card login-dark">
-            {/* {
+            {
                 statusAuthentication === "loading" &&
                     (
                         <Spinner style={{ width: '3rem', height: '3rem' }} color="primary" />
                     )
-            } */}
+            }
           </div>
         </Col>
       </Row>
