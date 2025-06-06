d421cd1fe4777337f988e1392ad7bebeeac7a2f7
MosesKal <mosesziongo@gmail.com>
2025-06-06 11:29:35 +0200

    feat: simplify TotalSells component by removing unused state and dispatches; focus on outreachers count

--- src/Components/cartographerAssistant/cartographerAssistantHomePage/TotalSells.tsx
+++ src/Components/cartographerAssistant/cartographerAssistantHomePage/TotalSells.tsx
@@ -1,53 +1,28 @@
 import {useEffect} from 'react';
 import Stat from '@/Components/Admin/AdminHomePage/Stat';
 import {useAppSelector, useAppDispatch} from "@/Redux/Hooks";
-import {fetchCall, fetchPublishedCall} from "@/Redux/Reducers/CallSlice";
-import {fetchUsers} from "@/Redux/Reducers/UserSlice";
-import {fetchStat} from '@/Redux/Reducers/StatSlice';
+import { fetchCountByOutreachers } from '@/Redux/Reducers/UserSlice';
 import {Row, Col} from "reactstrap";
 
 
 const TotalSells = () => {
 
-    const {totalAllCall, totalPublishedCall} = useAppSelector(state => state.call);
-    const {statData, status} = useAppSelector(state => state.stat);
-    const {totalUsers} = useAppSelector(state => state.user);
+    const {outReachersStatus, outReachersTotal } = useAppSelector(state => state.user);
     const dispatch = useAppDispatch();
 
     useEffect(
-        () => {
-            if (totalAllCall === null) {
-                dispatch(fetchCall());
+        ()=>{
+            if (outReachersStatus === "idle") {
+                dispatch(fetchCountByOutreachers());
             }
-            if (totalPublishedCall === null) {
-                dispatch(fetchPublishedCall());
-            }
-        },
-        [dispatch, totalAllCall, totalPublishedCall]
-    );
-
-    useEffect(
-        () => {
-            if (totalUsers === null) {
-                dispatch(fetchUsers());
-            }
-        },
-        [dispatch, totalUsers]
-    );
-
-    useEffect(() => {
-        if (status === 'idle') {
-            dispatch(fetchStat());
-        }
-    }, [dispatch, status]);
+        }, 
+        [outReachersStatus]
+    )
 
 
     return (
         <Row>
-            <Stat className={"total-sells"} title={"Nombre Total d'appels"} image={"opportunity.png"} count={totalAllCall} icon={"fa-arrow-up"} color={"success"}/>
-            <Stat className={"total-sells-2"} title={"Nombre Total d'appels publiés"} image={"bublishedOpportunity.png"} count={totalPublishedCall} icon={"fa-arrow-down"} color={"danger"}/>
-            <Stat className={"total-sells-4"} title={"Nombre Total d'utilisateurs"} image={"users.png"} count={totalUsers} icon={"fa-arrow-down"} color={"danger"}/>
-            <Stat className={"total-sells-3"} title={"Nombre Total de solutions"} image={"applications.png"} count={Number(statData.solutions)} icon={"fa-arrow-down"} color={"danger"}/>
+            <Stat className={"total-sells"} title={"Nombre Total de vulgarisateurs"} image={"users.png"} count={outReachersTotal} icon={"fa-arrow-up"} color={"success"}/>
         </Row>
     );
 };
